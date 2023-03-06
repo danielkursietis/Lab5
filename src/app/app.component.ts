3dbd3c51d1d936f8a7d5a46442b53f1ae171c0ba
unknown <g00381830@atu.ie>
2023-03-06 10:41:27 +0000

    lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-app';
+  count: number = 0;
+  hidden: boolean = true;
+
+  onClick(){
+    this.count++;
+  }
+
+  imgClick(){
+    switch (this.hidden){
+      case true: 
+        this.hidden = false;
+        break;
+      case false: 
+        this.hidden = true;
+        break;
+      default:
+        break;
+    }
+  
+  }
 }
